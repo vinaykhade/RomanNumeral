aacebfda98e0a2082ecd3e1503de7767195ef90e
vinaykhade <khadevnay.me@gmail.com>
2018-07-04 17:54:20 +0530

    added functionality to view converstion changes

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,35 +1,71 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { loadApp } from 'actions/app';
-import styles from './app.css';
+import styles from './app.scss';
+import RomanNumerals from './helper';
 
-type Props = {
-  dispatch: () => void,
-  loaded: boolean
-}
 
 export class AppContainer extends Component {
-  componentDidMount() {
-    this.props.dispatch(loadApp());
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      convertedValue: '',
+      inputError: ''
+    }
   }
 
-  props: Props;
+  handleChange = (event) => {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  getConvertedValue = (event) => {
+    event.preventDefault();
+
+    const { inputValue } = this.state;
+    const numberRegex = /^\d+$/;
+
+    if(numberRegex.test(inputValue)) {
+      this.setState({ 
+        convertedValue: RomanNumerals.toRoman(inputValue),
+        inputError: '' 
+      });
+    } else if(RomanNumerals.checkIfRomanNumeral(inputValue)) {
+      this.setState({ 
+        convertedValue: RomanNumerals.fromRoman(inputValue), 
+        inputError: ''
+      });
+    } else {
+      this.setState({ inputError: 'Please a valid input' })
+    }
+  }
 
   render() {
-    if (!this.props.loaded) {
-      return null;
-    }
+    const { convertedValue, inputError } = this.state;
 
     return (
-      <div className={styles.container} />
+      <div className={`container ${styles.container}`}>
+        <form className={styles.convertorForm} onSubmit={this.getConvertedValue}>
+          <div className={styles.formGroup}>
+            <label className={styles.formLabel}>Enter either a whole numeric value or a set of roman numerals below.</label>
+            <input
+              type="text"
+              className={styles.formInput}
+              onChange={this.handleChange}
+              placeholder="eg. 2018 or MMXVIII"
+              required
+            />
+            <p className={styles.errorMsg}>{inputError}</p>
+          </div>    
+          <button className={styles.submitBtn} type="submit">Convert</button>
+        </form>
+
+        <div className={styles.resultBox}>
+          <h2>Translation Result</h2>
+          <h1>{convertedValue}</h1>
+        </div>
+      </div>
     );
   }
 }
 
-function mapStateToProperties(state) {
-  return {
-    loaded: state.app.loaded
-  };
-}
-
-export default connect(mapStateToProperties)(AppContainer);
+export default AppContainer;
